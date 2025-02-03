c9f4a704afa46dfae1f280cdba8039218eb594e2
GitHub Action <action@github.com>
2025-02-03 12:14:12 +0000

    Initialize template repository proper
    Set package name to com.github.doeme07.gemstonecollection
    Set project name to GemstoneCollection
    Set modid to gemstonecollection

--- src/main/java/com/github/doeme07/gemstonecollection/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/doeme07/gemstonecollection/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.doeme07.gemstonecollection.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
